2acb142c3c7471463f82275fbd286dceaed7c221
Sudipto Das <eng.sudiptodas@gmail.com>
2026-07-15 20:39:13 +0600

    feat(auth): implement loginController with JWT session handling

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -78,11 +78,70 @@ const registerController = async (req, res) => {
   }
 };
 
+
+
+
+
 // ------- login controller --------
 const loginController = async (req, res) => {
   try {
-  } catch (error) {}
+    const { userName, email, password } = req.body;
+
+    // ----- required fields -------
+    const requiredFields = {
+      userName: 'UserName',
+      email: 'Email',
+      password: 'Password',
+    };
+    for (const [key, label] of Object.entries(requiredFields)) {
+      if (!req.body[key] || req.body[key]?.trim() === '') {
+        return res
+          .status(400)
+          .json({ success: false, message: `${label} is required` });
+      }
+    }
+
+    // ---- check existing user -----
+    const existingUser = await userModel.findOne({
+      $or: [{ email }, { userName }],
+    });
+    if (!existingUser) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Invalid Username/Email or Password' });
+    }
+
+    // ----- checking password -----
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+    if (!isMatch) {
+      return res.status(400).json({success: false, message:'Invalid password'})
+    }
+
+        // ----- token(7d) create ------
+    const token = jwt.sign({ userId: existingUser._id }, config.JSON_WEB_SECRET, {
+      expiresIn: '7d',
+    });
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: config.NODE_ENV === 'production',
+      sameSite: config.NODE_ENV === 'production' ? 'none' : 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({success: true, message: 'Login successful'})
+
+
+  } catch (error) {
+    res.status(500).json({success:false, message: error.message})
+  }
 };
+
+
+
+
+
+
 
 // ------------ check username availability controller -------------
 const checkUsernameAndEmailController = async (req, res) => {
@@ -117,5 +176,12 @@ const checkUsernameAndEmailController = async (req, res) => {
     res.status(500).json({ success: false, message: 'Internal server error' });
   }
 };
+
+
+
+
+
+
+
 
 export { registerController, loginController, checkUsernameAndEmailController };
